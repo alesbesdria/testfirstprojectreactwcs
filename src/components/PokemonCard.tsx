c9153236c6c71fb0d0c9aaf5ff30d29b83580161
alesbesdria <melissa@meirsman.fr>
2025-01-20 11:30:41 +0100

    work bulbasaur finish

---
+++ src/components/PokemonCard.tsx
@@ -0,0 +1,6 @@
+function PokemonCard() {
+    return     <div><figure><img src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png" alt="image Bulbasaur" /></figure>
+    <figcaption>Bulbasaur</figcaption></div>;
+  }
+  
+  export default PokemonCard;
