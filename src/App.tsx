27a5fdf019ca3946d20a3498a10f55ee0fa6e9f2
alesbesdria <melissa@meirsman.fr>
2025-01-21 17:01:48 +0100

    work 4 finish

--- src/App.tsx
+++ src/App.tsx
@@ -17,7 +17,7 @@ function App() {
 
   return (
     
-      <PokemonCard  pokemon={pokemonList[0]}/>
+      <PokemonCard  pokemon={pokemonList[1]}/>
     
   );
 }
--- src/components/PokemonCard.tsx
+++ src/components/PokemonCard.tsx
@@ -1,4 +1,4 @@
-function PokemonCard({pokemon}) {
+function PokemonCard({ pokemon }) {
 
 
     return (
